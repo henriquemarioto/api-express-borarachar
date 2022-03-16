fa2ba6cd89fabb4f0979a9613aea689f1b9efc3b
henriquemarioto <henrique.marioto@outlook.com>
2022-03-16 19:22:30 -0300

    add: authorization

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,13 @@
 import {Router} from 'express'
 import UserControllers from '../controllers/user.js'
+import { isAuthenticated } from "../middleware/auth.js"
 
 const routerUsers = Router()
 
-routerUsers.get("", (req, res) => UserControllers.getAllUsers(req, res))
-routerUsers.get("/:id", (req, res) => UserControllers.getUserById(req, res))
-routerUsers.patch("/:id", (req, res) => UserControllers.updateUser(req, res))
-routerUsers.delete("/:id", (req, res) => UserControllers.deleteUser(req, res))
+routerUsers.get("", isAuthenticated, UserControllers.getAllUsers)
+routerUsers.get("/:id", isAuthenticated, UserControllers.getUserById)
+routerUsers.patch("/:id", isAuthenticated, UserControllers.updateUser)
+routerUsers.delete("/:id", isAuthenticated, UserControllers.deleteUser)
 
 
 export default routerUsers
